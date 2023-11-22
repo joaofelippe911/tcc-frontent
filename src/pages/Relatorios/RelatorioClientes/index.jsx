53f8e4f618cfe963e168fbd4da972e3401d6950f
joaofelippe911 <joaofelippe0811@hotmail.com>
2023-11-21 21:16:48 -0300

    Ajusta url do relatorio de clientes que mais compraram

--- src/pages/Relatorios/RelatorioClientes/index.jsx
+++ src/pages/Relatorios/RelatorioClientes/index.jsx
@@ -2,6 +2,6 @@ import RelatorioForm from '../components/RelatorioForm';
 
 export default function RelatorioClientes() {
   return (
-    <RelatorioForm relatorio="relatorio-clientes" title="Relatório de clientes que mais compraram" /> 
+    <RelatorioForm relatorio="relatorio-clientes-que-mais-compraram" title="Relatório de clientes que mais compraram" /> 
   );
 }
